f0f6b018e0cf31b52a89f6a77db5b1757acca380
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-05-08 23:37:34 -0300

    algunas cositas

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export const Header = () => {
+    return (
+        <>
+            <h1>IDW</h1>
+            <img src="../src/assets/images/logo.png" alt="logo de la empresa" />
+            <h1>Alojamientos</h1>
+        </>
+    );
+}
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Sobre nosotros</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+    );  
+}
