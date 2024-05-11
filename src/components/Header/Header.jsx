c30e5a79b29ddb837074ebf9afd8fcc1e0ab3285
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-05-11 10:43:10 -0300

    header solved

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,17 +4,19 @@ import './header.css';
 
 export const Header = () => {
     return (
-        <div className='contenedorLogo'>
-            <div className='logo'>
-                <p className='logo-p1'>I<span className='quesi-queno-2'>D</span>W</p>
-                <p className='logo-p2'><span className='quesi-queno-2'>A</span>lo<span className='quesi-queno-1'>j</span>amien<span className='quesi-queno-3'>t</span>os</p>
-                <ul>
-                    <li className='quesi-queno-1'><i class="bi bi-house"></i></li>
-                    <li className='quesi-queno-2'><i class="bi bi-house-heart"></i></li>
-                    <li className='quesi-queno-3'><i class="bi bi-house"></i></li>
-                    <li className='quesi-queno-2'><i class="bi bi-house-heart"></i></li>
-                    <li className='quesi-queno-3'><i class="bi bi-house"></i></li>
-                </ul>
+        <div className='contenedorHeader'>
+            <div className='contenedorLogo '>
+                <div className='logo'>
+                    <p className='logo-p1'>I<span className='quesi-queno-2'>D</span>W</p>
+                    <p className='logo-p2'><span className='quesi-queno-2'>A</span>lo<span className='quesi-queno-1'>j</span>amien<span className='quesi-queno-3'>t</span>os</p>
+                    <ul>
+                        <li className='quesi-queno-1'><i class="bi bi-house"></i></li>
+                        <li className='quesi-queno-2'><i class="bi bi-house-heart"></i></li>
+                        <li className='quesi-queno-3'><i class="bi bi-house"></i></li>
+                        <li className='quesi-queno-2'><i class="bi bi-house-heart"></i></li>
+                        <li className='quesi-queno-3'><i class="bi bi-house"></i></li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
