dfdb56328f7c063859cf53676e6b071217e1beb3
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-05-11 03:22:48 -0300

    redes ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
   return (
     <div className='contenedorFooter'>
       <ul className='contenedorRedes'>
-        <li><a href="/#"><i class="bi bi-facebook"></i>Facebook</a></li>
-        <li><a href="/#"><i class="bi bi-instagram"></i>Instagram</a></li>
-        <li><a href="/#"><i class="bi bi-twitter-x"></i>Twitter-X</a></li>
+        <li><a className='quesi-queno-1 facebook' href="/#"><i class="bi bi-facebook"></i>Facebook</a></li>
+        <li><a className='quesi-queno-3 instagram' href="/#"><i class="bi bi-instagram"></i>Instagram</a></li>
+        <li><a className='quesi-queno-2 twitter-x' href="/#"><i class="bi bi-twitter-x"></i>Twitter-X</a></li>
       </ul>
       <p>
         &copy; 2024 IDW Alojamientos. Todos los derechos reservados.
