253375162d7aad9a13895eb3a9a5b07cef843802
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-05-11 10:47:20 -0300

    efecto en card

--- src/components/Body/AlojamientoCard/AlojamientoCard.jsx
+++ src/components/Body/AlojamientoCard/AlojamientoCard.jsx
@@ -9,9 +9,9 @@ export const AlojamientoCard = ({ card }) => {
         <a href=""><img src={card.imagenCover.img} alt="casa colonial" /></a>
         {/* Estos datos deben ser tomados de data.json */}
         <h3>{card.titulo}</h3>
-        <p><span>Localidad: </span>{card.ubicacion.latitud}, {card.ubicacion.longitud}</p>
-        <p><span>Disponibilidad: </span>{card.estado}</p>
-        <p><span>Precio por dia: $</span>{card.precioDia}</p>
+        <p><span className='quesi-queno-1'>Localidad: </span>{card.ubicacion.latitud}, {card.ubicacion.longitud}</p>
+        <p><span className='quesi-queno-3'>Disponibilidad: </span>{card.estado}</p>
+        <p><span className='quesi-queno-2'>Precio por dia: $</span>{card.precioDia}</p>
       </div>
     </figure>
   )
