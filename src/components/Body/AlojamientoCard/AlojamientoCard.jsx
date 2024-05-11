b7c701eee28b1a903c54546585ac059ced7a7f70
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-05-11 03:59:15 -0300

    wallpaper

--- src/components/Body/AlojamientoCard/AlojamientoCard.jsx
+++ src/components/Body/AlojamientoCard/AlojamientoCard.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import './AlojamientoCard.css'
 
 export const AlojamientoCard = ({ card }) => {
   console.log(card, "componenete AlojamientoCard")
   return (
-    <figure>
-      <a href=""><img src={card.imagenCover.img} alt="casa colonial" height={200} width={300} /></a>
-      {/* Estos datos deben ser tomados de data.json */}
-      <h3>{card.titulo}</h3>
-      <p><span>Localidad: </span>{card.ubicacion.latitud}, {card.ubicacion.longitud}</p>
-      <p><span>Disponibilidad: </span>{card.estado}</p>
-      <p><span>Precio por dia: $</span>{card.precioDia}</p>
+    <figure className='contenedorCard'>
+      <div className='card'>
+        <a href=""><img src={card.imagenCover.img} alt="casa colonial" /></a>
+        {/* Estos datos deben ser tomados de data.json */}
+        <h3>{card.titulo}</h3>
+        <p><span>Localidad: </span>{card.ubicacion.latitud}, {card.ubicacion.longitud}</p>
+        <p><span>Disponibilidad: </span>{card.estado}</p>
+        <p><span>Precio por dia: $</span>{card.precioDia}</p>
+      </div>
     </figure>
   )
 }
--- src/components/Body/GaleriaContenedor/GaleriaContenedor.jsx
+++ src/components/Body/GaleriaContenedor/GaleriaContenedor.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Galeria.css'
 import { AlojamientoContenedor } from '../AlojamientoContenedor/AlojamientoContenedor';
 
 export const GaleriaContenedor = () => {
 
     return (
-        <div>
+        <div className='contenedorGaleria'>
             <AlojamientoContenedor />
         </div>
     );
