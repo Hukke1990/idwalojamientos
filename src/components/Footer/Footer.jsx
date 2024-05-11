14460938b1e165f4ba4291cd4add32b57e4342a8
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-05-11 03:07:50 -0300

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      <ul>
+    <div className='contenedorFooter'>
+      <ul className='contenedorRedes'>
         <li><a href="/#"><i class="bi bi-facebook"></i>Facebook</a></li>
         <li><a href="/#"><i class="bi bi-instagram"></i>Instagram</a></li>
         <li><a href="/#"><i class="bi bi-twitter-x"></i>Twitter-X</a></li>
-        <p>
-            &copy; 2024 IDW Alojamientos. Todos los derechos reservados.
-        </p>
       </ul>
+      <p>
+        &copy; 2024 IDW Alojamientos. Todos los derechos reservados.
+      </p>
     </div>
   )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -27,7 +27,7 @@ export const Nav = () => {
                         <span className="span4"></span>
                         sobre nosotros
                     </a></li>
-                    <li><a href="#" class="btn">
+                    <li><a href="#" class="btn login">
                         <span className="span1"></span>
                         <span className="span2"></span>
                         <span className="span3"></span>
