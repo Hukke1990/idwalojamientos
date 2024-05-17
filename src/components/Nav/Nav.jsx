b05570b60cfbc31614c9f13ab04911160ae0e4d2
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-05-16 23:28:04 -0300

    todo OK

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import './Nav.css';
 import { NavLink } from 'react-router-dom';
 import Modal from '../Modal/ModalLogin';
 import useNavSticky from './NavSticky';
 
 export const Nav = () => {
-    const [modalOpen, setModalOpen] = useState(false);
+
+    const [modalOpen, setModalOpen] = useState(false); // Estado para controlar la apertura del modal
     const openModal = () => setModalOpen(true);
     const closeModal = () => setModalOpen(false);
 
@@ -14,34 +15,34 @@ export const Nav = () => {
     return (
         <nav className='contenedorMenu'>
             <div className="menu">
-                <ul className="nav-list">
+                <ul>
                     <li><NavLink exact to="/" activeClassName="active" className="btn home" id='home'>
                         <span className="span1"></span>
                         <span className="span2"></span>
                         <span className="span3"></span>
                         <span className="span4"></span>
-                        <i className="bi bi-house"></i>home {/* Icono de Bootstrap */}
+                        home
                     </NavLink></li>
                     <li><NavLink to="/Contact" activeClassName="active" className="btn contact">
                         <span className="span1"></span>
                         <span className="span2"></span>
                         <span className="span3"></span>
                         <span className="span4"></span>
-                        <i className="bi bi-envelope"></i>contacto {/* Icono de Bootstrap */}
+                        contacto
                     </NavLink></li>
                     <li><NavLink to="/About" activeClassName="active" className="btn">
                         <span className="span1"></span>
                         <span className="span2"></span>
                         <span className="span3"></span>
                         <span className="span4"></span>
-                        <i className="bi bi-info-circle"></i>sobre nosotros {/* Icono de Bootstrap */}
+                        sobre nosotros
                     </NavLink></li>
                     <button className="btn login" onClick={openModal}>
                         <span className="span1"></span>
                         <span className="span2"></span>
                         <span className="span3"></span>
                         <span className="span4"></span>
-                        <i className="bi bi-person"></i>login {/* Icono de Bootstrap */}
+                        login
                         <Modal isOpen={modalOpen} onClose={closeModal} /> {/* Agrega el modal aquí */}
                     </button>
                 </ul>
