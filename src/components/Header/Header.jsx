2a48a140eeeb80aa09f8d6f229515ec50a8e38c3
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-05-21 15:16:27 -0300

    acomodar logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,14 @@ export const Header = () => {
         <div className='contenedorHeader'>
             <div className='contenedorLogo '>
                 <div className='logo'>
-                    <p className='logo-p1'>I<span className='quesi-queno-2'>D</span>W</p>
-                    <p className='logo-p2'><span className='quesi-queno-2'>A</span>lo<span className='quesi-queno-1'>j</span>amien<span className='quesi-queno-3'>t</span>os</p>
+
+                    <div className='logo-p1'>
+                        <p className='logo-p1'>I<span className='quesi-queno-2'>D</span>W</p>
+                        <Logo />
+                        <p className='logo-p1'><span className='quesi-queno-2'>A</span>lo<span className='quesi-queno-1'>j</span>amien<span className='quesi-queno-3'>t</span>os</p>
+                    </div>
+                    <div className='reflection'></div>
+
                     <ul>
                         <li className=''><i class="bi bi-house-heart"></i></li>
                         <li className='quesi-queno-1'><i class="bi bi-house"></i></li>
@@ -19,6 +25,6 @@ export const Header = () => {
                     </ul>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
