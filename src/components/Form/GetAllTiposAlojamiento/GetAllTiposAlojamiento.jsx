200976aa0612ee6ecb7ecbcb0958892a831d4f48
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-06-04 16:05:29 -0300

    mejoras

--- src/components/Form/GetAllTiposAlojamiento/GetAllTiposAlojamiento.jsx
+++ src/components/Form/GetAllTiposAlojamiento/GetAllTiposAlojamiento.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
 import './GetAllTiposAlojamiento.css';
 
 export const GetAllTiposAlojamiento = () => {
