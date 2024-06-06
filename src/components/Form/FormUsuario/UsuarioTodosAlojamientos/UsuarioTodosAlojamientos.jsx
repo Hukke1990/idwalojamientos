ec6f997ae6d77587f45ad902e11552eb88e4424e
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-06-06 17:57:03 -0300

    solved boton volver

--- src/components/Form/FormUsuario/UsuarioTodosAlojamientos/UsuarioTodosAlojamientos.jsx
+++ src/components/Form/FormUsuario/UsuarioTodosAlojamientos/UsuarioTodosAlojamientos.jsx
@@ -12,21 +12,20 @@ export const UsuarioTodosAlojamientos = () => {
                 {alojamientos.map((alojamiento) => (
                     <li key={alojamiento.idAlojamiento}>
                         <h2>{alojamiento.Titulo}</h2>
-                        <p><span>ID:</span> {alojamiento.idAlojamiento}</p>
-                        <p><span>Descripcion:</span> {alojamiento.Descripcion}</p>
-                        <p><span>Tipo:</span> {alojamiento.TipoAlojamiento}</p>
-                        <p><span>Latitud:</span> {alojamiento.Latitud}</p>
-                        <p><span>Longitud:</span> {alojamiento.Longitud}</p>
-                        <p><span>Precio por día:</span> ${alojamiento.PrecioPorDia}</p>
-                        <p><span>Cantidad dormitorios:</span> {alojamiento.CantidadDormitorios}</p>
-                        <p><span>Cantidad baños:</span> {alojamiento.CantidadBanios}</p>
-                        <p><span>Estado:</span> {alojamiento.Estado}</p>
+                        <p><span className='label'>ID:</span> {alojamiento.idAlojamiento}</p>
+                        <p><span className='label'>Descripcion:</span> {alojamiento.Descripcion}</p>
+                        <p><span className='label'>Tipo:</span> {alojamiento.TipoAlojamiento}</p>
+                        <p><span className='label'>Latitud:</span> {alojamiento.Latitud}</p>
+                        <p><span className='label'>Longitud:</span> {alojamiento.Longitud}</p>
+                        <p><span className='label'>Precio por día:</span> ${alojamiento.PrecioPorDia}</p>
+                        <p><span className='label'>Cantidad dormitorios:</span> {alojamiento.CantidadDormitorios}</p>
+                        <p><span className='label'>Cantidad baños:</span> {alojamiento.CantidadBanios}</p>
+                        <p><span className='label'>Estado:</span> <span className={alojamiento.Estado === 'Disponible' ? 'estadoDisponible' : 'estadoReservado'}>{alojamiento.Estado}</span></p>
                     </li>
                 ))}
             </ul>
             {alertMessage && (
-                <div className={`alert ${alertType}`}>{alertMessage}
-                </div>
+                <div className={`alert ${alertType}`}>{alertMessage}</div>
             )}
         </div>
     );
