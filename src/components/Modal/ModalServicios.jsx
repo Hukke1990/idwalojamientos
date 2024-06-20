be7a06cc3168a301ef5daee4ae13cd86f99a762e
Hukke1990 <139823467+Hukke1990@users.noreply.github.com>
2024-06-20 01:00:53 -0300

    todo listo para entregar!

--- src/components/Modal/ModalServicios.jsx
+++ src/components/Modal/ModalServicios.jsx
@@ -3,17 +3,13 @@ import './ModalServicios.css'
 
 
 const Modal = ({ isOpen, onClose }) => {
-    // Lógica para cerrar el modal al presionar el botón de cerrar
     const handleModalClose = (e) => {
-        // Evitar la propagación del evento hacia arriba para que no se cierre el modal al hacer clic dentro de él
         e.stopPropagation();
-        // Lógica de cierre del modal al presionar el botón de cerrar
         onClose();
     };
 
-    // Verificar si el modal está abierto
     if (!isOpen) {
-        return null; // No renderizar nada si el modal está cerrado
+        return null;
     }
 
     return (
